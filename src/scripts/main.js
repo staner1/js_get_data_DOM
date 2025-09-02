f6e14c2d4902fc0a9b44d99ede3e278b783c86ef
Oleksandr <kozyrev.sania@gmail.com>
2025-09-02 17:20:40 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,60 +6,71 @@ function Calcs(values) {
   const results = [];
 
   let totalCalc = 0;
-
   let averageCalc = 0;
-
   let validCount = 0;
+
+  const separators = [',', '.', '\u00A0', '\u2009', ' '];
+  const sepCount = {};
+
+  for (const value of values) {
+    const text = value.innerText;
+
+    for (const sep of separators) {
+      if (text.includes(sep)) {
+        sepCount[sep] = (sepCount[sep] || 0) + 1;
+      }
+    }
+  }
+
+  let chosenSep = ',';
+  let maxCount = 0;
+
+  for (const [sep, count] of Object.entries(sepCount)) {
+    if (count > maxCount) {
+      maxCount = count;
+      chosenSep = sep;
+    }
+  }
 
   for (const value of values) {
     let text = value.innerText;
 
-    text = text.replaceAll(',', '');
+    for (const sep of separators) {
+      text = text.replaceAll(sep, '');
+    }
 
     const number = +text;
 
     if (!Number.isNaN(number)) {
       totalCalc = totalCalc + number;
-
       validCount++;
     }
   }
 
   averageCalc = Math.trunc(totalCalc / validCount);
 
-  function toStringCommas(value) {
-    let result = value.toString();
-
-    let separateNumbers = result.split('');
-
-    separateNumbers = separateNumbers.reverse();
+  function toStringSeparator(value, sep) {
+    const result = value.toString();
+    const separateNumbers = result.split('').reverse();
 
     let numberCount = 0;
-
-    const withCommas = [];
+    const withSep = [];
 
     for (let i = 0; i < separateNumbers.length; i++) {
-      withCommas.push(separateNumbers[i]);
-
+      withSep.push(separateNumbers[i]);
       numberCount++;
 
       if (numberCount === 3 && i !== separateNumbers.length - 1) {
-        withCommas.push(',');
-
+        withSep.push(sep);
         numberCount = 0;
       }
     }
 
-    separateNumbers = withCommas.reverse().join('');
-
-    result = separateNumbers;
-
-    return result;
+    return withSep.reverse().join('');
   }
 
-  totalCalc = toStringCommas(totalCalc);
-
-  averageCalc = toStringCommas(averageCalc);
+  totalCalc = toStringSeparator(totalCalc, chosenSep);
+  averageCalc = toStringSeparator(averageCalc, chosenSep);
 
   results.push(totalCalc, averageCalc);
 
